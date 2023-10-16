5ae366834a7ca3286d93c7155d6b066f35266c56
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-16 10:08:49 +0100

    Creation of images folder

--- src/components/navigationBar/navigationBar.js
+++ src/components/navigationBar/navigationBar.js
@@ -4,9 +4,18 @@ import './navigationBar.css';
 function navigationBar() {
     const [profileImage, setProfileImage] = useState('default-image.jpg');
 
-    const
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setProfileImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
   return (
-    <div>
+    <div className='navbar'>
       
     </div>
   )
