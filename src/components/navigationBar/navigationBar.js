3eebb8df4ae6d62054a1d734e26792afd71db9e9
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-16 09:51:54 +0100

    update dependencies

---
+++ src/components/navigationBar/navigationBar.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './navigationBar.css';
+
+function navigationBar() {
+    const [profileImage, setProfileImage] = useState('default-image.jpg');
+
+    const
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default navigationBar;
+
