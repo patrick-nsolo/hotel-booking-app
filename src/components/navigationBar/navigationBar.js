3c633a49cb2aa9a44d18c383ff26dba7c4944122
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-16 15:22:02 +0100

    update web app

--- src/components/navigationBar/navigationBar.js
+++ src/components/navigationBar/navigationBar.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { FaEdit, FaHome, FaCity, FaEnvelope, FaHeart, FaCog } from 'react-icons/fa';
 import './navigationBar.css';
 
 
 function NavigationBar() {
     const [profileImage, setProfileImage] = useState(null);
     const [isEditingImage, setIsEditingImage] = useState(false);
+    const [activeLink, setactiveLink] = useState(0);
+
 
     const handleImageUpload = (e) => {
         const file = e.target.files[0];
@@ -23,6 +26,11 @@ function NavigationBar() {
             document.getElementById('image-upload').click();
         }
     };
+
+    const handleClick = (index) => {
+        setActiveLink(index);
+    };
+
     return (
         <div className='navbar'>
             <div className='logo'>
@@ -32,13 +40,22 @@ function NavigationBar() {
                         <img src={profileImage} alt='Profile'/>
                     ):(
                         <div className='default-image'>
-
+                            <FaEdit/>
                         </div>
                     )}
                 </label>
-
+                <input type='file' id='image-upload' accept='image/*' onChange={handleImageUpload} style={{display:'none'}}/>
+                <h2 className='profile-name'>Ilia Jan</h2>
             </div>
-        
+            <div className='navigation'>
+                <ul className='nav-links'>
+                    <li className={`nav-item ${isClicked ? 'clicked' : ''}`} onClick={handleClick}><FaHome/>Dashboard</li>
+                    <li className='nav-item'><FaCity/>Explore City</li>
+                    <li className='nav-item'><FaEnvelope/>Ticket</li>
+                    <li className='nav-item'><FaHeart/>Favorites</li>
+                    <li className='nav-item'><FaCog/>Settings</li>
+                </ul>
+            </div>
         </div>
     )
 }
