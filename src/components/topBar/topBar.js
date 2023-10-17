9532cab98620d23b49116df40199f554df7c9814
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-17 11:20:49 +0100

    update

--- src/components/topBar/topBar.js
+++ src/components/topBar/topBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaSearch, FaBell } from 'react-icons/fa';
+
 import './TopBar.css';
 
 function TopBar() {
@@ -18,8 +19,18 @@ function TopBar() {
           <div className='notification-dot'></div>
         </div> 
       </div>
-      <div>
-        <img src='../images/hero-image.png' alt='couch'/>
+      <div className='image-container'>
+        <img src='/images/hero-image.png' alt='couch'/>
+        <div className='dropdown-container'>
+          <div className='location-dropdown'>
+              <input type='text' placeholder='Where are you going?'>
+                <option value="destination1">Destination 1</option>
+                <option value="destination2">Destination 2</option>
+                <option value="destination3">Destination 3</option>
+                {/* Add more destinations as needed */}
+              </input>
+          </div>
+        </div>
       </div>  
     </div>  
   )
